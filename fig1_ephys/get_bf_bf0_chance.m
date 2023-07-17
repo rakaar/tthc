b49b5a523ed32a9476770438a304e8b29f62a9f7
Raghavendra Kaushik <rka87338@gmail.com>
2023-07-17 12:47:31 +0530

    get bf bf0 chance in ephys and 2p

--- fig1_ephys/get_bf_bf0_chance.m
+++ fig1_ephys/get_bf_bf0_chance.m
@@ -5,29 +5,77 @@ rms_match_db = load('rms_match_db.mat').rms_match_db;
 bf_index = 12;
 bf0_index = 13;
 
-n_boots = 1000;
+n_boots = 10000;
 
 n_rows = size(rms_match_db,1);
-boots_distr = zeros(n_boots, 7, 7);
+chance_distr = zeros(n_boots, 7, 7);
+actual_distr = zeros(n_boots, 7, 7);
 
 for i = 1:n_boots
+    % bfs and bf0s
     all_bfs = cell2mat( rms_match_db(1:n_rows, bf_index) );
     all_bf0s = cell2mat( rms_match_db(1:n_rows, bf0_index) );
 
-    % shuffle both arrays with replacement 
-    all_bfs = all_bfs(randperm(length(all_bfs)));
-    all_bf0s = all_bf0s(randperm(length(all_bf0s)));
+    % actual distr 
+    indices = randi(length(all_bfs), length(all_bfs), 1);
+    all_bfs_shuffle = all_bfs(indices);
+    all_bf0s_shuffle = all_bf0s(indices);
+    actual_distr(i,:,:) = get_bf_bf0_mat(all_bfs_shuffle, all_bf0s_shuffle);
 
-    chance_bf_bf0_mat = get_bf_bf0_mat(all_bfs, all_bf0s);
-    boots_distr(i,:,:) = chance_bf_bf0_mat;
+    % chance distr
+    indices1 = randi(length(all_bfs), length(all_bfs), 1);
+    indices2 = randi(length(all_bfs), length(all_bfs), 1);
+    all_bfs_shuffle = all_bfs(indices1);
+    all_bf0s_shuffle = all_bf0s(indices2);
+    chance_distr(i,:,:) = get_bf_bf0_mat(all_bfs_shuffle, all_bf0s_shuffle);
 end
 
-% actual dist 
-all_bfs = cell2mat( rms_match_db(1:n_rows, bf_index) );
-all_bf0s = cell2mat( rms_match_db(1:n_rows, bf0_index) );
-actual_bf_bf0_mat = get_bf_bf0_mat(all_bfs, all_bf0s);
+% norms comparison
+% symmetric_deviation = norm(A - (A + A')/2);
 
-% actual_bf_bf0_mat(7 x 7) and chance_bf_bf0_mat(n_boots x 7 x 7) 
+chance_symmetric_devs = zeros(n_boots, 1);
+actual_symmetric_devs = zeros(n_boots, 1);
+
+for i = 1:n_boots
+    % get chance matrix
+    chance_mat = squeeze(chance_distr(i,:,:));
+    % normalize by sum of all elemetns
+    chance_mat = chance_mat ./ sum(chance_mat(:));
+    % get symmetric deviation
+    chance_symmetric_devs(i) = norm(chance_mat - (chance_mat + chance_mat')/2);
+
+    % get actual matrix
+    actual_mat = squeeze(actual_distr(i,:,:));
+    % normalize by sum of all elemetns
+    actual_mat = actual_mat ./ sum(actual_mat(:));
+    % get symmetric deviation
+    actual_symmetric_devs(i) = norm(actual_mat - (actual_mat + actual_mat')/2);
+end
+
+% confidence interval comparison
+confidence_interval = 0.90;
+lower_bound = round((1 - confidence_interval)/2 * n_boots);
+upper_bound = round((1 - (1 - confidence_interval)/2) * n_boots);
+
+% get confidence interval of both chance and actual symmetric dev
+chance_symmetric_devs = sort(chance_symmetric_devs);
+actual_symmetric_devs = sort(actual_symmetric_devs);
+
+chance_ci = [chance_symmetric_devs(lower_bound), chance_symmetric_devs(upper_bound)];
+actual_ci = [actual_symmetric_devs(lower_bound), actual_symmetric_devs(upper_bound)];
+
+% display both cis
+disp('chance ci:');
+disp(chance_ci);
+disp('actual ci:');
+disp(actual_ci);
+
+if actual_ci(1) > chance_ci(2)
+    disp('✔ significant difference');
+else
+    disp('✘ no significant difference');
+end
+
 
 function bf_bf0_mat = get_bf_bf0_mat(all_bfs, all_bf0s)
     bf_bf0_mat = zeros(7,7);

a13dff60baa0ce3af555a7eda21662c3d4597616
Raghavendra Kaushik <rka87338@gmail.com>
2023-07-19 20:53:23 +0530

    boots pca of pv som from thy

---
+++ connectivity_2p/pca_of_pv_som_boots.m
@@ -0,0 +1,159 @@
+clear;clc;close all;
+% Scores of rebf vs dist matrices of pv, som, thy on thy basis
+disp('Boots- Running pca of pv and som on thy basis')
+
+% load matrices
+thy_chance = load('thy_chance').chance_all_pairs_rebf_vs_dist_boots;
+pv_chance = load('pv_chance').chance_all_pairs_rebf_vs_dist_boots;
+som_chance = load('som_chance').chance_all_pairs_rebf_vs_dist_boots;
+
+thy_actual = load('thy_actual').actual_all_pairs_rebf_vs_dist_boots;
+pv_actual = load('pv_actual').actual_all_pairs_rebf_vs_dist_boots;
+som_actual = load('som_actual').actual_all_pairs_rebf_vs_dist_boots;
+
+n_boots = 1000;
+chance_pv_from_thy = zeros(n_boots, 1);
+chance_som_from_thy = zeros(n_boots, 1);
+
+actual_pv_from_thy = zeros(n_boots, 1);
+actual_som_from_thy = zeros(n_boots, 1);
+
+for b = 1:n_boots
+
+    disp(['Boots: ' num2str(b)])
+    % chance
+    thy = squeeze(thy_chance(b, 1:6, 1:7));
+    pv = squeeze(pv_chance(b, 1:6, 1:7));
+    som = squeeze(som_chance(b, 1:6, 1:7));
+
+    % replace nan with zeros in all 3
+    thy(isnan(thy)) = 0;
+    pv(isnan(pv)) = 0;
+    som(isnan(som)) = 0;
+
+    % do pca of thy
+    [coeff,score,latent] = pca(thy);
+
+    
+    % NOTE thy - mean(thy) = score x coeff'
+
+    % Get scores of pv and som on thy basis
+    % pv - mean(pv) = score_pv_on_thy_basis x coeff'
+    % Multiply by coeff on both sides
+    % (pv - mean(pv))*coeff = score_pv_on_thy_basis x coeff'coeff
+    % coeff'coeff = 1
+    % (pv - mean(pv))*coeff = score_pv_on_thy_basis
+
+    score_pv_on_thy_basis = (pv - mean(pv))*coeff;
+    score_som_on_thy_basis = (som - mean(som))*coeff;
+    score_thy_on_thy_basis = score;
+
+    % visualise take only first 3
+    % Assuming A, B, and C are your 6 x 5 matrices
+    % Take first 3 columns from each matrix
+    A = score_thy_on_thy_basis;
+    B = score_pv_on_thy_basis;
+    C = score_som_on_thy_basis;
+
+    % Assuming A, B, and C are your 6 x 5 matrices
+    % Take first 3 columns from each matrix
+    A = A(:, 1:3);
+    B = B(:, 1:3);
+    C = C(:, 1:3);
+
+
+    % get distances of pv and som from thy (B,C from A)
+    % euclidean distance
+    dist_pv_from_thy = sqrt(sum((B - A).^2, 2));
+    dist_som_from_thy = sqrt(sum((C - A).^2, 2));
+
+    chance_pv_from_thy(b) = mean(dist_pv_from_thy);
+    chance_som_from_thy(b) = mean(dist_som_from_thy);
+
+    % actual
+    thy = squeeze(thy_actual(b, 1:6, 1:7));
+    pv = squeeze(pv_actual(b, 1:6, 1:7));
+    som = squeeze(som_actual(b, 1:6, 1:7));
+
+    % replace nan with zeros in all 3
+    thy(isnan(thy)) = 0;
+    pv(isnan(pv)) = 0;
+    som(isnan(som)) = 0;
+
+    % do pca of thy
+    [coeff,score,latent] = pca(thy);
+
+    
+    % NOTE thy - mean(thy) = score x coeff'
+
+    % Get scores of pv and som on thy basis
+    % pv - mean(pv) = score_pv_on_thy_basis x coeff'
+    % Multiply by coeff on both sides
+    % (pv - mean(pv))*coeff = score_pv_on_thy_basis x coeff'coeff
+    % coeff'coeff = 1
+    % (pv - mean(pv))*coeff = score_pv_on_thy_basis
+
+    score_pv_on_thy_basis = (pv - mean(pv))*coeff;
+    score_som_on_thy_basis = (som - mean(som))*coeff;
+    score_thy_on_thy_basis = score;
+
+    % visualise take only first 3
+    % Assuming A, B, and C are your 6 x 5 matrices
+    % Take first 3 columns from each matrix
+    A = score_thy_on_thy_basis;
+    B = score_pv_on_thy_basis;
+    C = score_som_on_thy_basis;
+
+    % Assuming A, B, and C are your 6 x 5 matrices
+    % Take first 3 columns from each matrix
+    A = A(:, 1:3);
+    B = B(:, 1:3);
+    C = C(:, 1:3);
+
+
+    % get distances of pv and som from thy (B,C from A)
+    % euclidean distance
+    dist_pv_from_thy = sqrt(sum((B - A).^2, 2));
+    dist_som_from_thy = sqrt(sum((C - A).^2, 2));
+
+    actual_pv_from_thy(b) = mean(dist_pv_from_thy);
+    actual_som_from_thy(b) = mean(dist_som_from_thy);
+
+
+end % b
+
+
+
+% confidence interval test
+% PV
+actual_pv_from_thy = sort(actual_pv_from_thy);
+chance_pv_from_thy = sort(chance_pv_from_thy);
+
+confidence = 0.90;
+actual_ci = actual_pv_from_thy([floor((1-confidence)/2*n_boots) ceil((1+confidence)/2*n_boots)]);
+chance_ci = chance_pv_from_thy([floor((1-confidence)/2*n_boots) ceil((1+confidence)/2*n_boots)]);
+
+if actual_ci(1) > chance_ci(2)
+    disp('🔥 PV: Actual is greater than chance')
+else
+    disp('👻PV: Actual is not greater than chance')
+end
+disp(['chance ci: ' num2str(chance_ci(1)) ' ' num2str(chance_ci(2))])
+disp(['actual ci: ' num2str(actual_ci(1)) ' ' num2str(actual_ci(2))])
+
+% SOM
+actual_som_from_thy = sort(actual_som_from_thy);
+chance_som_from_thy = sort(chance_som_from_thy);
+
+confidence = 0.90;
+actual_ci = actual_som_from_thy([floor((1-confidence)/2*n_boots) ceil((1+confidence)/2*n_boots)]);
+chance_ci = chance_som_from_thy([floor((1-confidence)/2*n_boots) ceil((1+confidence)/2*n_boots)]);
+
+if actual_ci(1) > chance_ci(2)
+    disp('🔥 SOM: Actual is greater than chance')
+else
+    disp('👻SOM: Actual is not greater than chance')
+end
+
+disp(['chance ci: ' num2str(chance_ci(1)) ' ' num2str(chance_ci(2))])
+disp(['actual ci: ' num2str(actual_ci(1)) ' ' num2str(actual_ci(2))])
---
+++ connectivity_2p/re_bf_dist_conn_boots.m
@@ -0,0 +1,333 @@
+% Bootstrap re BF vs num of connected pairs
+clear;clc;close all;
+disp('Running re_bf_dist_conn.m - conn vs rebf vs dist')
+neuron_type = 'Thy'; % PV or SOM or Thy
+rms_match_db_with_sig_bf = load("E:\RK_E_folder_TTHC_backup\RK TTHC Data\"+ neuron_type + "\rms_match_db_with_sig_bf.mat").rms_match_db_with_sig_bf;
+rms_match_db = load("E:\RK_E_folder_TTHC_backup\RK TTHC Data\" + neuron_type + "\rms_match_db.mat").rms_match_db;
+
+
+% add 2 columns - x coord and y coord
+for u=1:size(rms_match_db_with_sig_bf,1)
+   rms_match_db_with_sig_bf{u,17} = rms_match_db{u,10};
+   rms_match_db_with_sig_bf{u,18} = rms_match_db{u,11};
+end
+
+% seperate gender wise
+animal_gender = 'all';
+if strcmp(animal_gender, 'M')
+    rejected_gender = 'F';
+elseif strcmp(animal_gender, 'F')
+    rejected_gender = 'M';
+else
+    rejected_gender = nan;
+end
+if ~isnan(rejected_gender)
+    removal_indices = [];
+    for u = 1:size(rms_match_db_with_sig_bf,1)
+        animal_name = rms_match_db_with_sig_bf{u,1};
+        if contains(animal_name, strcat('_', rejected_gender))
+            removal_indices = [removal_indices; u];
+        end
+    end
+
+    rms_match_db_with_sig_bf(removal_indices,:) = [];
+end
+
+
+% BF or BF0
+scale = 'BF'; % 11-BF or 13-BF0
+if strcmp(scale, 'BF')
+    bf_index = 11;
+elseif strcmp(scale, 'BF0')
+    bf_index = 13;
+end
+
+% remove rows where 11 or 13th column of cell is -1
+removal_indices = [];
+for u=1:size(rms_match_db_with_sig_bf,1)
+    if rms_match_db_with_sig_bf{u,11} == -1 || rms_match_db_with_sig_bf{u,13} == -1
+        removal_indices = [removal_indices;u];
+    end
+end
+rms_match_db_with_sig_bf(removal_indices,:) = [];
+
+% Fill noise corr vecs
+for u=1:size(rms_match_db_with_sig_bf,1)
+    tbf = rms_match_db_with_sig_bf{u,11};
+    hbf = rms_match_db_with_sig_bf{u,13};
+
+   
+
+    % tone
+    rate_ind = 8;
+    noise_ind = 14;
+    noise = [];
+    for freq=1:7
+        r5 = mean(rms_match_db_with_sig_bf{u,rate_ind}{freq,1}(:,10:14),2);
+        n5 = r5 - mean(r5);
+        noise = [noise; n5];
+    end
+
+    rms_match_db_with_sig_bf{u,noise_ind} = noise;
+
+    % hc
+    rate_ind = 9;
+    noise_ind = 15;
+    noise = [];
+    for freq=1:7
+        r5 = mean(rms_match_db_with_sig_bf{u,rate_ind}{freq,1}(:,10:14),2);
+        n5 = r5 - mean(r5);
+        noise = [noise; n5];
+    end
+    rms_match_db_with_sig_bf{u,noise_ind} = noise;
+
+
+end
+
+% get all with same roi
+roi_to_units_map = containers.Map;
+combiner = '***';
+for u = 1:size(rms_match_db_with_sig_bf)
+    animal_name = rms_match_db_with_sig_bf{u,1};
+    location = rms_match_db_with_sig_bf{u,2};
+    spl_str = num2str(rms_match_db_with_sig_bf{u,6});
+
+    roi_uniq_name = strcat(animal_name,combiner,location,combiner,spl_str);
+    if isKey(roi_to_units_map,roi_uniq_name)
+        roi_to_units_map(roi_uniq_name) = [roi_to_units_map(roi_uniq_name);u];
+    else
+        roi_to_units_map(roi_uniq_name) = [u];
+    end
+end
+
+% get each roi info in 1 struct
+noise_index = 14; % TONE Noise Correlations
+
+all_roi_keys = keys(roi_to_units_map);
+counter = 1;
+
+all_roi_info = struct('roi_name', {}, 'rebf', {}, 'x_coord', {}, 'y_coord', {}, 'noise_corr_mat', {});
+
+for k = 1:length(all_roi_keys)
+    roi_key_name = all_roi_keys{k};
+    roi_units = roi_to_units_map(roi_key_name);
+    n_roi_units = length(roi_units);
+
+    roi_name = roi_key_name;
+    roi_re_bf = zeros(n_roi_units, 1);
+    roi_x_coord = zeros(n_roi_units, 1);
+    roi_y_coord = zeros(n_roi_units, 1);
+    roi_noise_corr_mat = zeros(n_roi_units, n_roi_units);
+
+    roi_noise_corr_vecs = zeros(35, n_roi_units);
+    
+    for ru = 1:length(roi_units)
+        unit_index = roi_units(ru);
+        
+        % fill rebf
+        roi_re_bf(ru,1) = rms_match_db_with_sig_bf{unit_index, bf_index};
+        % coords
+        roi_x_coord(ru,1) = rms_match_db_with_sig_bf{unit_index, 17};
+        roi_y_coord(ru,1) = rms_match_db_with_sig_bf{unit_index, 18};
+
+        %   nc
+        roi_noise_corr_vecs(:,ru) = rms_match_db_with_sig_bf{unit_index, noise_index}; 
+    end % ru
+
+    roi_noise_corr_vecs = corrcoef(roi_noise_corr_vecs);
+
+
+    all_roi_info(counter).roi_name = roi_name;
+    all_roi_info(counter).rebf = roi_re_bf;
+    all_roi_info(counter).x_coord = roi_x_coord;
+    all_roi_info(counter).y_coord = roi_y_coord;
+    all_roi_info(counter).noise_corr_mat = roi_noise_corr_vecs;
+
+    counter = counter + 1;
+end % k
+
+% Code to get upp tri elements
+% A = [1 2 3; 4 5 6; 7 8 9];
+% mask = triu(true(size(A)), 1);
+% B = A(mask);
+% disp(B);
+
+total_pairs_len = 0;
+for i = 1:length(all_roi_info)
+    n_cells = length(all_roi_info(i).rebf);
+    total_pairs_len = total_pairs_len + (n_cells*(n_cells-1))/2;
+end % i
+
+% all pairs rebf, dist, noise corr
+all_pairs_noise_corr = [];
+for i = 1:length(all_roi_info)
+    roi_noise_corr = all_roi_info(i).noise_corr_mat;
+    mask = triu(true(size(roi_noise_corr)), 1);
+    triu_noise_corr = roi_noise_corr(mask);
+
+    all_pairs_noise_corr = [all_pairs_noise_corr; triu_noise_corr];
+end % i
+
+% Get the left and right extreme
+mean_noise_corr = mean(all_pairs_noise_corr);
+std_noise_corr = std(all_pairs_noise_corr);
+right_extreme = mean_noise_corr + 2*std_noise_corr;
+left_extreme = mean_noise_corr - 2*std_noise_corr;
+
+% get the matrix
+dist_bin_size = 50;
+max_dist = 500; % microns
+dist_bins = 0:dist_bin_size:max_dist-dist_bin_size;
+rebf_bins = 0:0.5:3;
+
+% all pairs rebf vs dist
+all_pairs_rebf_vs_dist = zeros(length(dist_bins), length(rebf_bins));
+
+% bootstrapped data
+n_boots = 1000;
+chance_all_pairs_rebf_vs_dist_boots = zeros(n_boots, length(dist_bins), length(rebf_bins));
+actual_all_pairs_rebf_vs_dist_boots = zeros(n_boots, length(dist_bins), length(rebf_bins));
+
+for b = 1:n_boots
+    disp(['Boot: ' num2str(b)])
+    % actual data
+    for i = 1:length(all_roi_info)
+        
+        all_bf = all_roi_info(i).rebf;
+
+        % same indicies to shuffle 
+        random_indices = randperm(length(all_bf));
+        % shuffle bf
+        all_bf = all_bf(random_indices);
+
+        % shuffle positions
+        all_x_cord = all_roi_info(i).x_coord;
+        all_x_cord = all_x_cord(random_indices);
+
+        all_y_cord = all_roi_info(i).y_coord;
+        all_y_cord = all_y_cord(random_indices);
+
+        all_noise_corr = all_roi_info(i).noise_corr_mat;
+        % all_noise_corr = all_noise_corr(random_indices, random_indices);
+
+        n_cells = length(all_bf);
+        for n1 = 1:n_cells-1
+            for n2 = n1+1:n_cells
+                nc_for_n1_n2 = all_noise_corr(n1,n2);
+                if nc_for_n1_n2 > right_extreme || nc_for_n1_n2 < left_extreme
+                    % get the distance
+                    x1 = all_x_cord(n1);
+                    y1 = all_y_cord(n1);
+                    x2 = all_x_cord(n2);
+                    y2 = all_y_cord(n2);
+                    dist = sqrt((x1-x2)^2 + (y1-y2)^2)*1.17; % 1.17 microns per pixel
+                    dist_bin = floor(dist/dist_bin_size) + 1;
+    
+                    % get the rebf
+                    bf1 = all_bf(n1);
+                    bf2 = all_bf(n2);
+                    rebf = abs(bf1 - bf2)*0.5;
+                    rebf_bin = find(rebf_bins == rebf);
+    
+    
+                    actual_all_pairs_rebf_vs_dist_boots(b, dist_bin, rebf_bin) = actual_all_pairs_rebf_vs_dist_boots(b, dist_bin, rebf_bin) + 1;
+                end 
+            end
+        end
+    end % i
+
+    % chance
+    for i = 1:length(all_roi_info)
+        
+        all_bf = all_roi_info(i).rebf;
+        % for bf
+        random_indices = randperm(length(all_bf));
+        % shuffle bf
+        all_bf = all_bf(random_indices);
+
+        % for distances
+        random_indices = randperm(length(all_bf));
+        all_x_cord = all_roi_info(i).x_coord;
+        % shuffle x cord
+        all_x_cord = all_x_cord(random_indices);
+
+        all_y_cord = all_roi_info(i).y_coord;
+        % shuffle y cord
+        all_y_cord = all_y_cord(random_indices);
+
+        % for nc
+        random_indices = randperm(length(all_bf));
+        all_noise_corr = all_roi_info(i).noise_corr_mat;
+        % shuffle nc
+        % all_noise_corr = all_noise_corr(random_indices, random_indices);
+    
+        n_cells = length(all_bf);
+        for n1 = 1:n_cells-1
+            for n2 = n1+1:n_cells
+                nc_for_n1_n2 = all_noise_corr(n1,n2);
+                if nc_for_n1_n2 > right_extreme || nc_for_n1_n2 < left_extreme
+                    % get the distance
+                    x1 = all_x_cord(n1);
+                    y1 = all_y_cord(n1);
+                    x2 = all_x_cord(n2);
+                    y2 = all_y_cord(n2);
+                    dist = sqrt((x1-x2)^2 + (y1-y2)^2)*1.17; % 1.17 microns per pixel
+                    dist_bin = floor(dist/dist_bin_size) + 1;
+    
+                    % get the rebf
+                    bf1 = all_bf(n1);
+                    bf2 = all_bf(n2);
+                    rebf = abs(bf1 - bf2)*0.5;
+                    rebf_bin = find(rebf_bins == rebf);
+    
+    
+                    chance_all_pairs_rebf_vs_dist_boots(b, dist_bin, rebf_bin) = chance_all_pairs_rebf_vs_dist_boots(b, dist_bin, rebf_bin) + 1;
+                end 
+            end
+        end
+    end % i
+    
+    
+end % b
+
+
+% threshold for actual
+% threshold = 5;
+% for b = 1:n_boots
+%     for r = 1:length(dist_bins)
+%         for c = 1:length(rebf_bins)
+%             if length(actual_all_pairs_rebf_vs_dist_boots(b,r,c)) < threshold
+%                 actual_all_pairs_rebf_vs_dist_boots(b,r,c) = nan;
+%             end
+%         end
+%     end
+% end % b
+
+% % threshold for chance
+% for b = 1:n_boots
+%     for r = 1:length(dist_bins)
+%         for c = 1:length(rebf_bins)
+%             if length(chance_all_pairs_rebf_vs_dist_boots(b,r,c)) < threshold
+%                 chance_all_pairs_rebf_vs_dist_boots(b,r,c) = nan;
+%             end
+%         end
+%     end
+% end % b
+
+
+% normalise each row in actual by nansum of each row
+for b = 1:n_boots
+    for r = 1:length(dist_bins)
+        actual_all_pairs_rebf_vs_dist_boots(b,r,:) = actual_all_pairs_rebf_vs_dist_boots(b,r,:)/nansum(actual_all_pairs_rebf_vs_dist_boots(b,r,:));
+    end
+end % b
+
+% normalise each row in chance by nansum of each row
+for b = 1:n_boots
+    for r = 1:length(dist_bins)
+        chance_all_pairs_rebf_vs_dist_boots(b,r,:) = chance_all_pairs_rebf_vs_dist_boots(b,r,:)/nansum(chance_all_pairs_rebf_vs_dist_boots(b,r,:));
+    end
+end % b
+
+save(strcat(neuron_type, '_chance.mat'), 'chance_all_pairs_rebf_vs_dist_boots');
+save(strcat(neuron_type, '_actual.mat'), 'actual_all_pairs_rebf_vs_dist_boots');

da181834102df52d373ea880fe433da8eca6e538
Raghavendra Kaushik <rka87338@gmail.com>
2023-07-26 13:26:15 +0530

    2p code to generate he hs cum sum plot

--- fig1_ephys/he_hs_as_func_of_re_bf_gender_wise.m
+++ fig1_ephys/he_hs_as_func_of_re_bf_gender_wise.m
@@ -10,83 +10,85 @@ clear;clc;close all;
 % end
 % rms_match_db(removal_indices,:) = [];
 
-rms_match_db = load('rms_match_db.mat').rms_match_db;
-% decide only male or female
-animal_gender = 'F'; % M for Male, F for Female, all for both
-if strcmp(animal_gender, 'M')
-    rejected_gender = 'F';
-elseif strcmp(animal_gender, 'F')
-    rejected_gender = 'M';
-else
-    rejected_gender = nan;
-end
-if ~isnan(rejected_gender)
-    removal_indices = [];
-    for u = 1:size(rms_match_db,1)
-        animal_name = rms_match_db{u,1};
-        % if animal name includes _{rejected_gender} add it to removal index
-        if contains(animal_name, strcat('_',rejected_gender))
-            removal_indices = [removal_indices; u];
-        end
-    end % u
-
-    % remove rejected gender
-    rms_match_db(removal_indices,:) = [];
-
-end % if
-
-
+all_gender = {'M', 'F'};
 octaves_apart = -3:0.5:3;
 n_octaves_apart = length(octaves_apart);
 
-num_cases_base_re_bf = zeros(n_octaves_apart,4); % base how far from BF
-num_cases_second_comp_re_bf = zeros(n_octaves_apart,4); % second comp from BF
-num_cases_min_re_bf = zeros((n_octaves_apart-1)/2 + 1, 4); % min distance of either components from BF
-
+num_cases_base_re_bf = zeros(2, n_octaves_apart,4); % base how far from BF
+% 2 - M,F 3 - octaves apart, 4 - HE, HS, NE, NS
 bf_index = 12; % 12 for BF, 13 for BF0
 
-for u = 1:size(rms_match_db,1)
-    bf = rms_match_db{u,bf_index};
-    if bf == -1
-        continue
-    end
-
-    % for u = 1:1
-    all_tone_rates = rms_match_db{u,6};
-    all_hc_rates = rms_match_db{u,7};
-    all_tone_sig = rms_match_db{u,8};
-    all_hc_sig = rms_match_db{u,9};
-
-    
-    for f = 1:5
-        t1_spike_rates = mean(all_tone_rates{f,1}(:, 501:570),2);
-        t2_spike_rates = mean(all_tone_rates{f+2,1}(:,501:570),2);
-        t1t2_spike_rates = mean(all_hc_rates{f,1}(:, 501:570), 2);
-
-        t1_mean_rate = mean(t1_spike_rates);
-        t2_mean_rate = mean(t2_spike_rates);
-        t1t2_mean_rate = mean(t1t2_spike_rates);
-
-        indiv_type = indiv_he_hs_classifier(t1_spike_rates, t2_spike_rates, t1t2_spike_rates, all_tone_sig(f), all_tone_sig(f+2), all_hc_sig(f));
-        if isnan(indiv_type)
-           disp('HE HS cant be nan')
-            return
+for gender_no = 1:2
+    rms_match_db = load('rms_match_db.mat').rms_match_db;
+        % decide only male or female
+        animal_gender = all_gender{gender_no}; % M for Male, F for Female, all for both
+        if strcmp(animal_gender, 'M')
+            rejected_gender = 'F';
+        elseif strcmp(animal_gender, 'F')
+            rejected_gender = 'M';
+        else
+            rejected_gender = nan;
         end
+        if ~isnan(rejected_gender)
+            removal_indices = [];
+            for u = 1:size(rms_match_db,1)
+                animal_name = rms_match_db{u,1};
+                % if animal name includes _{rejected_gender} add it to removal index
+                if contains(animal_name, strcat('_',rejected_gender))
+                    removal_indices = [removal_indices; u];
+                end
+            end % u
+
+            % remove rejected gender
+            rms_match_db(removal_indices,:) = [];
+
+        end % if
 
 
-        base_oct_apart_from_bf = (f - bf)*0.5;
-        second_comp_oct_apart_from_bf = (f + 2 - bf)*0.5;
-        
-        base_index = 7 + 2*base_oct_apart_from_bf;
-        second_comp_index = 7 + 2*second_comp_oct_apart_from_bf;
-        min_index = 1 + 2*min(abs(base_oct_apart_from_bf), abs(second_comp_oct_apart_from_bf));
 
-        num_cases_base_re_bf(base_index, indiv_type) = num_cases_base_re_bf(base_index, indiv_type) + 1;
-        num_cases_second_comp_re_bf(second_comp_index, indiv_type) = num_cases_second_comp_re_bf(second_comp_index, indiv_type) + 1;
-        num_cases_min_re_bf(min_index, indiv_type) = num_cases_min_re_bf(min_index, indiv_type) + 1;
-        
-    end
-end % u
+        for u = 1:size(rms_match_db,1)
+            bf = rms_match_db{u,bf_index};
+            if bf == -1
+                continue
+            end
+
+            % for u = 1:1
+            all_tone_rates = rms_match_db{u,6};
+            all_hc_rates = rms_match_db{u,7};
+            all_tone_sig = rms_match_db{u,8};
+            all_hc_sig = rms_match_db{u,9};
+
+            
+            for f = 1:5
+                t1_spike_rates = mean(all_tone_rates{f,1}(:, 501:570),2);
+                t2_spike_rates = mean(all_tone_rates{f+2,1}(:,501:570),2);
+                t1t2_spike_rates = mean(all_hc_rates{f,1}(:, 501:570), 2);
+
+                t1_mean_rate = mean(t1_spike_rates);
+                t2_mean_rate = mean(t2_spike_rates);
+                t1t2_mean_rate = mean(t1t2_spike_rates);
+
+                indiv_type = indiv_he_hs_classifier(t1_spike_rates, t2_spike_rates, t1t2_spike_rates, all_tone_sig(f), all_tone_sig(f+2), all_hc_sig(f));
+                if isnan(indiv_type)
+                disp('HE HS cant be nan')
+                    return
+                end
+
+
+                base_oct_apart_from_bf = (f - bf)*0.5;
+                second_comp_oct_apart_from_bf = (f + 2 - bf)*0.5;
+                
+                base_index = 7 + 2*base_oct_apart_from_bf;
+                second_comp_index = 7 + 2*second_comp_oct_apart_from_bf;
+                min_index = 1 + 2*min(abs(base_oct_apart_from_bf), abs(second_comp_oct_apart_from_bf));
+
+                num_cases_base_re_bf(gender_no, base_index, indiv_type) = num_cases_base_re_bf(gender_no, base_index, indiv_type) + 1;
+            end
+        end % u
+
+end % gender_no
+
+
 
 % plot
 type_strs = {'HE', 'HS', 'NE', 'NS'};
@@ -95,46 +97,19 @@ if bf_index == 12
 else
     bf_str = 'BF0';
 end
+
 figure
     for i = 1:4
         subplot(2,2,i)
-        bar(octaves_apart, num_cases_base_re_bf(:,i))
-        title(type_strs{i})
+        both_m_f_each_cat_data = squeeze(num_cases_base_re_bf(:,:,i))';
+        for j = 1:2
+            both_m_f_each_cat_data(:,j) = both_m_f_each_cat_data(:,j)./sum(both_m_f_each_cat_data(:,j));
+        end
+        bar(octaves_apart, both_m_f_each_cat_data, 'grouped')
         xlabel(['Base octaves apart from BF/BF0 - scale ' bf_str])
         ylabel('Number of cases')
         title(type_strs{i})
-    end
-figure
-    for i = 1:4
-        subplot(2,2,i)
-        bar(octaves_apart, num_cases_base_re_bf(:,i)./sum(num_cases_base_re_bf(:,i)))
-        title(type_strs{i})
-        xlabel(['Base octaves apart from BF/BF0 - scale ' bf_str])
-        ylabel('Number of cases')
-        title(type_strs{i})
+        legend('M', 'F')
     end
 
-num_cases_base_re_bf_norm = zeros(n_octaves_apart,4);
-for i = 1:4
-    num_cases_base_re_bf_norm(:,i) = num_cases_base_re_bf(:,i)./sum(num_cases_base_re_bf(:,i));
-end
-save(strcat(animal_gender, '_hehs_rebf'), 'num_cases_base_re_bf_norm')
-% figure
-%     for i = 1:4
-%         subplot(2,2,i)
-%         bar(octaves_apart, num_cases_second_comp_re_bf(:,i))
-%         title(type_strs{i})
-%         xlabel(['Second octaves apart from BF/BF0 - scale ' bf_str])
-%         ylabel('Number of cases')
-%         title(type_strs{i})
-%     end
 
-% figure
-%     for i = 1:4
-%         subplot(2,2,i)
-%         bar(0:0.5:3, num_cases_min_re_bf(:,i))
-%         title(type_strs{i})
-%         xlabel(['Min octaves apart from BF/BF0 - scale ' bf_str])
-%         ylabel('Number of cases')
-%         title(type_strs{i})
-%     end
--- fig234_2p/he_hs_as_func_of_rebf_gender_wise.m
+++ fig234_2p/he_hs_as_func_of_rebf_gender_wise.m
@@ -1,9 +1,6 @@
 % Number of cases of HE, HS as a func of re BF
 clear;clc; 
 disp('Running he_hs_as_func_of_re_bf.m to find he hs as a function of re BF')
-rms_match_file_path = 'E:\RK_E_folder_TTHC_backup\RK TTHC Data\Thy\rms_match_db_with_sig_bf.mat';
-rms_match_db_with_sig_bf = load(rms_match_file_path).rms_match_db_with_sig_bf;
-rms_match_db = rms_match_db_with_sig_bf;
 
 % remove non-20 dB from all rms values
 % removal_indices = [];
@@ -13,115 +10,176 @@ rms_match_db = rms_match_db_with_sig_bf;
 %     end
 % end
 % rms_match_db(removal_indices,:) = [];
-% decide only male or female
-animal_gender = 'F'; % M for Male, F for Female, all for both
-if strcmp(animal_gender, 'M')
-    rejected_gender = 'F';
-elseif strcmp(animal_gender, 'F')
-    rejected_gender = 'M';
-else
-    rejected_gender = nan;
-end
-if ~isnan(rejected_gender)
-    removal_indices = [];
-    for u = 1:size(rms_match_db,1)
-        animal_name = rms_match_db{u,1};
-        % if animal name includes _{rejected_gender} add it to removal index
-        if contains(animal_name, strcat('_',rejected_gender))
-            removal_indices = [removal_indices; u];
-        end
-    end % u
 
-    % remove rejected gender
-    rms_match_db(removal_indices,:) = [];
-
-end % if
-
-
+all_gender = {'M', 'F'};
+all_neuron_types = {'PV', 'SOM', 'Thy'};
+all_bf_indices = [11,13];
 octaves_apart = -3:0.5:3;
 n_octaves_apart = length(octaves_apart);
+num_cases_base_re_bf = zeros(2, n_octaves_apart,4); % base how far from BF
 
+% 2 - M,F 3 - octaves apart, 4 - HE, HS, NE, NS
 
-num_cases_base_re_bf = zeros(n_octaves_apart,4); % base how far from BF
+% decide only male or female
 
-bf_index = 11; % 11 for BF, 13 for BF0
+for n = 1:length(all_neuron_types)
+    for b = 1:length(all_bf_indices)
+        for gender_no = 1:2
+            neuron_type = all_neuron_types{n};
+            bf_index = all_bf_indices(b); % 11 for BF, 13 for BF0
 
-for u = 1:size(rms_match_db,1)
-    bf = rms_match_db{u,bf_index};
-    if bf == -1
-        continue
-    end
-
-    % for u = 1:1
-    all_tone_rates = rms_match_db{u,8};
-    all_hc_rates = rms_match_db{u,9};
-    all_tone_sig = rms_match_db{u,10};
-    all_hc_sig = rms_match_db{u,12};
-
-    all_tone_sig(find(all_tone_sig == -1)) = 0;
-    all_hc_sig(find(all_hc_sig == -1)) = 0;
-
-    if anynan(all_tone_sig) || anynan(all_hc_sig)
-        continue
-    end
-
-    
-    for f = 1:5
-        t1_spike_rates = mean(all_tone_rates{f,1}(:, 10:14),2);
-        t2_spike_rates = mean(all_tone_rates{f+2,1}(:,10:14),2);
-        t1t2_spike_rates = mean(all_hc_rates{f,1}(:, 10:14), 2);
-
-        t1_mean_rate = mean(t1_spike_rates);
-        t2_mean_rate = mean(t2_spike_rates);
-        t1t2_mean_rate = mean(t1t2_spike_rates);
-
-        indiv_type = indiv_he_hs_classifier(t1_spike_rates, t2_spike_rates, t1t2_spike_rates, all_tone_sig(f), all_tone_sig(f+2), all_hc_sig(f));
-        if isnan(indiv_type)
-           disp('HE HS cant be nan')
-            return
+            rms_match_file_path = strcat('E:\RK_E_folder_TTHC_backup\RK TTHC Data\', neuron_type ,'\rms_match_db_with_sig_bf.mat');
+            rms_match_db_with_sig_bf = load(rms_match_file_path).rms_match_db_with_sig_bf;
+            rms_match_db = rms_match_db_with_sig_bf;
+        
+            animal_gender = all_gender{gender_no}; % M for Male, F for Female, all for both
+            if strcmp(animal_gender, 'M')
+                rejected_gender = 'F';
+            elseif strcmp(animal_gender, 'F')
+                rejected_gender = 'M';
+            else
+                rejected_gender = nan;
+            end
+            if ~isnan(rejected_gender)
+                removal_indices = [];
+                for u = 1:size(rms_match_db,1)
+                    animal_name = rms_match_db{u,1};
+                    % if animal name includes _{rejected_gender} add it to removal index
+                    if contains(animal_name, strcat('_',rejected_gender))
+                        removal_indices = [removal_indices; u];
+                    end
+                end % u
+        
+                % remove rejected gender
+                rms_match_db(removal_indices,:) = [];
+        
+            end % if
+        
+            for u = 1:size(rms_match_db,1)
+                bf = rms_match_db{u,bf_index};
+                if bf == -1
+                    continue
+                end
+        
+                % for u = 1:1
+                all_tone_rates = rms_match_db{u,8};
+                all_hc_rates = rms_match_db{u,9};
+                all_tone_sig = rms_match_db{u,10};
+                all_hc_sig = rms_match_db{u,12};
+        
+                all_tone_sig(find(all_tone_sig == -1)) = 0;
+                all_hc_sig(find(all_hc_sig == -1)) = 0;
+        
+                if anynan(all_tone_sig) || anynan(all_hc_sig)
+                    continue
+                end
+        
+                
+                for f = 1:5
+                    t1_spike_rates = mean(all_tone_rates{f,1}(:, 10:14),2);
+                    t2_spike_rates = mean(all_tone_rates{f+2,1}(:,10:14),2);
+                    t1t2_spike_rates = mean(all_hc_rates{f,1}(:, 10:14), 2);
+        
+                    t1_mean_rate = mean(t1_spike_rates);
+                    t2_mean_rate = mean(t2_spike_rates);
+                    t1t2_mean_rate = mean(t1t2_spike_rates);
+        
+                    indiv_type = indiv_he_hs_classifier(t1_spike_rates, t2_spike_rates, t1t2_spike_rates, all_tone_sig(f), all_tone_sig(f+2), all_hc_sig(f));
+                    if isnan(indiv_type)
+                    disp('HE HS cant be nan')
+                        return
+                    end
+        
+        
+                    base_oct_apart_from_bf = (f - bf)*0.5;
+                    second_comp_oct_apart_from_bf = (f + 2 - bf)*0.5;
+                    
+                    base_index = 7 + 2*base_oct_apart_from_bf;
+                    second_comp_index = 7 + 2*second_comp_oct_apart_from_bf;
+                    min_index = 1 + 2*min(abs(base_oct_apart_from_bf), abs(second_comp_oct_apart_from_bf));
+        
+                    num_cases_base_re_bf(gender_no, base_index, indiv_type) = num_cases_base_re_bf(gender_no, base_index, indiv_type) + 1;
+                    
+                end
+            end % u
+        
+        end % gender_no
+        
+        type_strs = {'HE', 'HS', 'NE', 'NS'};
+        
+        if bf_index == 11
+            bf_str = 'BF';
+        else
+            bf_str = 'BF0';
         end
 
-
-        base_oct_apart_from_bf = (f - bf)*0.5;
-        second_comp_oct_apart_from_bf = (f + 2 - bf)*0.5;
+        % chi sq tests
+        disp(['Neuron Type = ' neuron_type ' BF index = ' bf_str ])
+        for cat = 1:4
+            male_data = num_cases_base_re_bf(1,:,cat);
+            female_data = num_cases_base_re_bf(2,:,cat);
         
-        base_index = 7 + 2*base_oct_apart_from_bf;
-        second_comp_index = 7 + 2*second_comp_oct_apart_from_bf;
-        min_index = 1 + 2*min(abs(base_oct_apart_from_bf), abs(second_comp_oct_apart_from_bf));
-
-        num_cases_base_re_bf(base_index, indiv_type) = num_cases_base_re_bf(base_index, indiv_type) + 1;
+            % direct chi square
+            % [h,p] = do_chi_sq(male_data, female_data);
+            % disp('Direct chi square')
+            % disp(['h = ' num2str(h) ' p = ' num2str(p) ' for ' type_strs{cat}])
+        
+            % % add +0.5 to both male and female
+            % [h,p] = do_chi_sq(male_data + 0.5, female_data + 0.5);
+            % disp('+0.5')
+            % disp(['h = ' num2str(h) ' p = ' num2str(p) ' for ' type_strs{cat}])
+        
+        
+             % kstest
+             male_data_for_kstest = [];
+             female_data_for_kstest = [];
+             for i = 1:n_octaves_apart
+                male_data_for_kstest = [male_data_for_kstest octaves_apart(i)*ones(1,male_data(i))];
+                female_data_for_kstest = [female_data_for_kstest octaves_apart(i)*ones(1, female_data(i))];
+            end
+            [h,p] = kstest2(male_data_for_kstest, female_data_for_kstest);
+             disp('kstest2 ')
+             disp(['h = ' num2str(h) ' p = ' num2str(p) ' for ' type_strs{cat}])
+         
+            
+        end % cat
+        
+        
+        % plot
+        
+        
+        
+        figure
+            for i = 1:4
+                subplot(2,2,i)
+                both_m_f_each_cat_data = squeeze(num_cases_base_re_bf(:,:,i))';
+                for j = 1:2
+                    both_m_f_each_cat_data(:,j) = both_m_f_each_cat_data(:,j)./sum(both_m_f_each_cat_data(:,j));
+                end
+                bar(octaves_apart, both_m_f_each_cat_data, 'grouped')
+                xlabel(['Base octaves apart from BF/BF0 - scale ' bf_str])
+                ylabel('Prop of cases')
+                title(type_strs{i})
+                legend('M', 'F')
+            end
+        
+        figure
+            for i = 1:4
+                subplot(2,2,i)
+                both_m_f_each_cat_data = squeeze(num_cases_base_re_bf(:,:,i))';
+                for j = 1:2
+                    both_m_f_each_cat_data(:,j) = both_m_f_each_cat_data(:,j)./sum(both_m_f_each_cat_data(:,j));
+                end
+                hold on
+                    plot(cumsum(both_m_f_each_cat_data(:,1)), 'LineWidth', 2, 'Color', 'b')
+                    plot(cumsum(both_m_f_each_cat_data(:,2)), 'LineWidth', 2, 'Color', 'r')
+                hold off
+                xlabel(['Base octaves apart from BF/BF0 - scale ' bf_str])
+                ylabel('Cum sum of prop')
+                title([type_strs{i} ' ' neuron_type])
+                legend('M', 'F')
+            end
         
     end
-end % u
+end % n
 
-% plot
-type_strs = {'HE', 'HS', 'NE', 'NS'};
-if bf_index == 11
-    bf_str = 'BF';
-else
-    bf_str = 'BF0';
-end
-% figure
-%     for i = 1:4
-%         subplot(2,2,i)
-%         bar(octaves_apart, num_cases_base_re_bf(:,i))
-%         title(type_strs{i})
-%         xlabel(['Base octaves apart from BF/BF0 - scale ' bf_str])
-%         ylabel('Number of cases')
-%         title(type_strs{i})
-%     end
-figure
-    for i = 1:4
-        subplot(2,2,i)
-        bar(octaves_apart, num_cases_base_re_bf(:,i)./sum(num_cases_base_re_bf(:,i)))
-        title(type_strs{i})
-        xlabel(['Base octaves apart from BF/BF0 - scale ' bf_str])
-        ylabel('Number of cases')
-        title(type_strs{i})
-    end
-
-num_cases_base_re_bf_norm = zeros(n_octaves_apart,4);
-for i = 1:4
-    num_cases_base_re_bf_norm(:,i) = num_cases_base_re_bf(:,i)./sum(num_cases_base_re_bf(:,i));
-end
-save(strcat(animal_gender, '_hehs_rebf'), 'num_cases_base_re_bf_norm')

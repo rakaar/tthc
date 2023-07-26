fcc6d3dc73785aba4aa385b3c70631a687880f3a
Raghavendra Kaushik <rka87338@gmail.com>
2023-07-26 18:55:16 +0530

    example raster and tuning

--- fig1_ephys/see_raster_for_he_hs.m
+++ fig1_ephys/see_raster_for_he_hs.m
@@ -11,6 +11,8 @@ for u = 1:size(rms_match_db,1)
 end
 rms_match_db(removal_indices,:) = [];
 
+no_effect_indices = [];
+
 example_no1 = nan;
 all_neurons_frac_types = struct('he', {}, 'hs', {}, 'ne', {}, 'ns', {});
 for u = 1:size(rms_match_db,1)
@@ -54,7 +56,7 @@ for u = 1:size(rms_match_db,1)
 
     if n_ne >= 4
         example_no3 = u;
-        % disp(u)
+        no_effect_indices = [no_effect_indices u];
     end
     
     all_neurons_frac_types(u).he = n_he;
@@ -65,7 +67,8 @@ for u = 1:size(rms_match_db,1)
 
 end % u
 
-
+% manual inspection 
+example_no3 = 94; % else 21
 
 % plot raster of example neuron
 % 1
@@ -97,7 +100,8 @@ for f = 1:5:35
     freq_labels(f) = strcat(num2str(all_freqs((f-1)/5 + 1)), ' Hz');
 end
 
-marker_size = 4;
+marker_size = 2;
+
 figure
 subplot(2,1,1)
     for iStim = 1:35
@@ -112,7 +116,7 @@ subplot(2,1,1)
     xlabel('Time (s)');
     yticks(1:35); % Set y-axis ticks
     yticklabels(freq_labels); % Set y-axis tick labels
-    title('Tone Raster Plot');
+    title(['Tone Raster Plot ' num2str(example_no) ]);
     hold off
 
 subplot(2,1,2)
@@ -130,6 +134,8 @@ subplot(2,1,2)
     yticklabels(freq_labels); % Set y-axis tick labels
     xlabel('Time (s)');
     title('HC Raster Plot');
+
+    saveas(gcf, 'E:\RK_E_folder_TTHC_backup\RK TTHC figs eps\fig1\HE_raster.fig')
 
 
 % 2
@@ -161,7 +167,7 @@ for f = 1:5:35
     freq_labels(f) = strcat(num2str(all_freqs((f-1)/5 + 1)), ' Hz');
 end
 
-marker_size = 4;
+marker_size = 2;
 figure
 subplot(2,1,1)
     for iStim = 1:35
@@ -176,7 +182,7 @@ subplot(2,1,1)
     xlabel('Time (s)');
     yticks(1:35); % Set y-axis ticks
     yticklabels(freq_labels); % Set y-axis tick labels
-    title('Tone Raster Plot');
+    title(['Tone Raster Plot ' num2str(example_no) ]);
     hold off
 
 subplot(2,1,2)
@@ -194,9 +200,10 @@ subplot(2,1,2)
     yticklabels(freq_labels); % Set y-axis tick labels
     xlabel('Time (s)');
     title('HC Raster Plot');
-
+    saveas(gcf, 'E:\RK_E_folder_TTHC_backup\RK TTHC figs eps\fig1\HS_raster.fig')
 
 % 3
+
 example_no = example_no3;
 time_period = 501:570;
 all_tone_dots = zeros(35, length(time_period));
@@ -225,7 +232,7 @@ for f = 1:5:35
     freq_labels(f) = strcat(num2str(all_freqs((f-1)/5 + 1)), ' Hz');
 end
 
-marker_size = 4;
+marker_size = 2;
 figure
 subplot(2,1,1)
     for iStim = 1:35
@@ -240,7 +247,7 @@ subplot(2,1,1)
     xlabel('Time (s)');
     yticks(1:35); % Set y-axis ticks
     yticklabels(freq_labels); % Set y-axis tick labels
-    title('Tone Raster Plot');
+    title(['Tone Raster Plot ' num2str(example_no) ]);
     hold off
 
 subplot(2,1,2)
@@ -258,6 +265,7 @@ subplot(2,1,2)
     yticklabels(freq_labels); % Set y-axis tick labels
     xlabel('Time (s)');
     title('HC Raster Plot');
+    saveas(gcf, 'E:\RK_E_folder_TTHC_backup\RK TTHC figs eps\fig1\NE_raster.fig')
 % tuning curves
 % 1
 example_no = example_no1;
@@ -286,10 +294,10 @@ figure
     legend('Tone', 'HC');
     xlabel('Frequency (kHz)');
     ylabel('Spike Rate (spikes/s)');
-    title('Tuning Curves');
+    title(['Tuning Curves '  num2str(example_no)]);
     hold off
     xticklabels({'6', '8.5', '12', '17', '24', '34', '48'});
-
+    saveas(gcf, 'E:\RK_E_folder_TTHC_backup\RK TTHC figs eps\fig1\HE_tuning.fig')
 % 2
 example_no = example_no2;
 
@@ -317,11 +325,13 @@ figure
     legend('Tone', 'HC');
     xlabel('Frequency (kHz)');
     ylabel('Spike Rate (spikes/s)');
-    title('Tuning Curves');
+    title(['Tuning Curves '  num2str(example_no)]);
     hold off
     xticklabels({'6', '8.5', '12', '17', '24', '34', '48'});
+    saveas(gcf, 'E:\RK_E_folder_TTHC_backup\RK TTHC figs eps\fig1\HS_tuning.fig')
 
 % 3 
+
 example_no = example_no3;
 
 all_tone_rates = rms_match_db{example_no,6};
@@ -348,6 +358,118 @@ figure
     legend('Tone', 'HC');
     xlabel('Frequency (kHz)');
     ylabel('Spike Rate (spikes/s)');
-    title('Tuning Curves');
+    title(['Tuning Curves '  num2str(example_no)]);
     hold off
     xticklabels({'6', '8.5', '12', '17', '24', '34', '48'});
+    saveas(gcf, 'E:\RK_E_folder_TTHC_backup\RK TTHC figs eps\fig1\NE_tuning.fig')
+
+    %%  TEMP
+    % close all
+    
+% for i = 1:length(no_effect_indices)
+%     example_no = no_effect_indices(i);
+
+%     all_tone_rates = rms_match_db{example_no,6};
+%     all_hc_rates = rms_match_db{example_no,7};
+
+%     tone_mean = zeros(7,1);
+%     tone_err = zeros(7,1);
+%     hc_mean = zeros(7,1);
+%     hc_err = zeros(7,1);
+%     for i = 1:7
+%         t_rates = 1000.*mean(all_tone_rates{i,1}(:, 501:570),2);
+%         h_rates = 1000.*mean(all_hc_rates{i,1}(:, 501:570),2);
+
+%         tone_mean(i) = mean(t_rates);
+%         tone_err(i) = std(t_rates)/sqrt(length(t_rates));
+%         hc_mean(i) = mean(h_rates);
+%         hc_err(i) = std(h_rates)/sqrt(length(h_rates));
+%     end
+    
+%         hold on
+%             errorbar(1:7, tone_mean, tone_err, 'LineWidth', 2, 'Color', 'b');
+%             errorbar(1:7, hc_mean, hc_err, 'LineWidth', 2, 'Color', 'r');
+%         hold off
+%         legend('Tone', 'HC');
+%         xlabel('Frequency (kHz)');
+%         ylabel('Spike Rate (spikes/s)');
+%         title(num2str(example_no));
+%         hold off
+%         xticklabels({'6', '8.5', '12', '17', '24', '34', '48'});
+
+%         pause
+%        clf
+
+% end
+
+
+% see raster
+% raster_egs = [6 13 21 87 94 104 140 145];
+% for i = 1:length(raster_egs)
+%     example_no = raster_egs(i);
+%     marker_size = 2;
+
+%     time_period = 501:570;
+%     all_tone_dots = zeros(35, length(time_period));
+%     all_hc_dots = zeros(35, length(time_period));
+
+%     all_tone_rates = rms_match_db{example_no,6};
+%     all_hc_rates = rms_match_db{example_no,7};
+
+%     for iter = 1:5
+%         for f = 1:7
+%             ind35 = (iter - 1)*7 + f;
+            
+%             tone_f_iter_spikes = all_tone_rates{f,1}(iter, time_period);
+%             hc_f_iter_spikes = all_hc_rates{f,1}(iter, time_period);
+
+%             all_tone_dots(ind35,:) = tone_f_iter_spikes;
+%             all_hc_dots(ind35,:) = hc_f_iter_spikes;
+
+            
+%         end % f
+%     end % iter
+
+%     all_freqs = [6, 8.5, 12, 17, 24, 34, 48];
+%     freq_labels = strings(35,1);
+%     for f = 1:5:35
+%         freq_labels(f) = strcat(num2str(all_freqs((f-1)/5 + 1)), ' Hz');
+%     end
+
+
+% subplot(2,1,1)
+%     for iStim = 1:35
+%         spikeTime = find(all_tone_dots(iStim,:) == 1);
+%         y = iStim * ones(size(spikeTime));  % Same y for all spikes from one stimulus
+%         plot(spikeTime, y, 'ko', 'MarkerSize', marker_size, 'MarkerFaceColor', 'b'); hold on;   % Plot the spikes as dots
+%     end
+
+%     ylim([0 35]); % set y limits
+%     xlim([0 70]);
+%     ylabel('Stimulus');
+%     xlabel('Time (s)');
+%     yticks(1:35); % Set y-axis ticks
+%     yticklabels(freq_labels); % Set y-axis tick labels
+%     title(['Tone Raster Plot ' num2str(example_no)]);
+%     hold off
+
+% subplot(2,1,2)
+%     for iStim = 1:35
+%         spikeTime = find(all_hc_dots(iStim,:) == 1);
+%         y = iStim * ones(size(spikeTime));  % Same y for all spikes from one stimulus
+%         plot(spikeTime, y, 'ko', 'MarkerSize', marker_size, 'MarkerFaceColor', 'r');
+%         hold on;   % Plot the spikes as dots
+%     end
+
+%     ylim([0 35]); % set y limits
+%     xlim([0 70]);
+%     ylabel('Stimulus');
+%     yticks(1:35); % Set y-axis ticks
+%     yticklabels(freq_labels); % Set y-axis tick labels
+%     xlabel('Time (s)');
+%     title('HC Raster Plot');
+
+%     pause
+%     clf
+
+% end 

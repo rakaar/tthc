5d2d956506570411e8a203844fa3dc865a9bdf7b
Raghavendra Kaushik <rka87338@gmail.com>
2023-08-03 09:31:43 +0530

    num of units /total sig in su

---
+++ fig1_ephys/count_num_out_of_total.m
@@ -0,0 +1,172 @@
+clear;close all;clc
+% 1 - M,F 2 - 0:10:40, 3 - resp, 4 - Total
+% ----------------- Tone ---------------------
+data = load('E:\RK_code_TTHC\ephys_new_db\tone_db_stage1.mat').tone_db_stage1;
+
+all_dbs = unique(cell2mat(data(:,5)));
+
+num_data = zeros(2, length(all_dbs),2);
+
+for u = 1:size(data,1)
+    resp = data{u,6};
+    spont = [];
+    for f = 1:7
+        spont = [spont; mean(resp{f,1}(:, 431:500),2)];
+    end
+    
+    t_res = zeros(7,1);
+    for f = 1:7
+        f_res = mean(resp{f,1}(:, 501:570),2);
+        t_res(f) = ttest2(f_res, spont);
+    end
+
+    animal_name = data{u,1};
+    spl = data{u,5};
+    
+    if contains(animal_name, '_M')
+        gender_index = 1;
+    elseif contains(animal_name, '_F')
+        gender_index = 2;
+    end
+
+
+    spl_index = find(all_dbs == spl);
+
+    num_data(gender_index, spl_index, 2) =  num_data(gender_index, spl_index, 2) + 1;
+    if sum(t_res) > 0
+        num_data(gender_index, spl_index, 1) =  num_data(gender_index, spl_index, 1) + 1;
+    end
+
+
+end
+
+disp('#### Tone Data ####')
+disp('% of units responding to atleast 1 freq ')
+table_data = num_data(:,:,1)./num_data(:,:,2)*100;
+% disp(table_data)
+
+% Convert matrix to table
+T = array2table(table_data);
+
+% Add column names
+T.Properties.VariableNames =  string(all_dbs);
+
+% Add row names
+T.Properties.RowNames = {'Male', 'Female'};
+
+% Display the table
+disp(T)
+
+disp('# of units responding to atleast 1 freq ')
+table_data = squeeze(num_data(:,:,1));
+% Convert matrix to table
+T = array2table(table_data);
+
+% Add column names
+T.Properties.VariableNames =  string(all_dbs);
+
+% Add row names
+T.Properties.RowNames = {'Male', 'Female'};
+
+% Display the table
+disp(T)
+
+disp('# of total units')
+table_data = squeeze(num_data(:,:,2));
+
+% Convert matrix to table
+T = array2table(table_data);
+
+% Add column names
+T.Properties.VariableNames =  string(all_dbs);
+
+% Add row names
+T.Properties.RowNames = {'Male', 'Female'};
+
+% Display the table
+disp(T)
+% ----------------------- HC -------------------------
+data = load('E:\RK_code_TTHC\ephys_new_db\hc_db_stage1.mat').hc_db_stage1;
+
+all_dbs = unique(cell2mat(data(:,5)));
+
+num_data = zeros(2, length(all_dbs),2);
+
+for u = 1:size(data,1)
+    resp = data{u,6};
+    spont = [];
+    for f = 1:7
+        spont = [spont; mean(resp{f,1}(:, 431:500),2)];
+    end
+    
+    t_res = zeros(7,1);
+    for f = 1:7
+        f_res = mean(resp{f,1}(:, 501:570),2);
+        t_res(f) = ttest2(f_res, spont);
+    end
+
+    animal_name = data{u,1};
+    spl = data{u,5};
+    
+    if contains(animal_name, '_M')
+        gender_index = 1;
+    elseif contains(animal_name, '_F')
+        gender_index = 2;
+    end
+
+
+    spl_index = find(all_dbs == spl);
+
+    num_data(gender_index, spl_index, 2) =  num_data(gender_index, spl_index, 2) + 1;
+    if sum(t_res) > 0
+        num_data(gender_index, spl_index, 1) =  num_data(gender_index, spl_index, 1) + 1;
+    end
+
+
+end
+
+disp('#### HC Data ####')
+disp('% of units responding to atleast 1 freq ')
+table_data = num_data(:,:,1)./num_data(:,:,2)*100;
+% disp(table_data)
+
+% Convert matrix to table
+T = array2table(table_data);
+
+% Add column names
+T.Properties.VariableNames =  string(all_dbs);
+
+% Add row names
+T.Properties.RowNames = {'Male', 'Female'};
+
+% Display the table
+disp(T)
+
+disp('# of units responding to atleast 1 freq ')
+table_data = squeeze(num_data(:,:,1));
+% Convert matrix to table
+T = array2table(table_data);
+
+% Add column names
+T.Properties.VariableNames =  string(all_dbs);
+
+% Add row names
+T.Properties.RowNames = {'Male', 'Female'};
+
+% Display the table
+disp(T)
+
+disp('# of total units')
+table_data = squeeze(num_data(:,:,2));
+
+% Convert matrix to table
+T = array2table(table_data);
+
+% Add column names
+T.Properties.VariableNames =  string(all_dbs);
+
+% Add row names
+T.Properties.RowNames = {'Male', 'Female'};
+
+% Display the table
+disp(T)
---
+++ fig234_2p/count_mf_db_wise_units.m
@@ -0,0 +1,42 @@
+clear;clc; close all;
+rms_match_db = load('E:\RK_E_folder_TTHC_backup\RK TTHC Data\PV\rms_match_db_with_sig_bf.mat').rms_match_db_with_sig_bf;
+
+dbs = [0,10,20,30,40];
+num_of_units = zeros(2, length(dbs), 3);
+% 1,2 - M,F
+% 3 - Only T, Only HC, Both T and HC
+
+for u = 1:size(rms_match_db,1)
+    animal_name = rms_match_db{u,1};
+    spl = rms_match_db{u,6};
+
+    if contains(animal_name, '_M')
+        gender_index = 1;
+    elseif contains(animal_name, '_F')
+        gender_index = 2;
+    end
+
+    spl_index = find(dbs == spl);
+
+    if rms_match_db{u,11} ~= -1 && rms_match_db{u,13} == -1
+        resp_index = 1;
+    elseif rms_match_db{u,11} == -1 && rms_match_db{u,13} ~= -1
+        resp_index = 2;
+    elseif rms_match_db{u,11} ~= -1 && rms_match_db{u,13} ~= -1
+        resp_index = 3;
+    end
+
+
+    num_of_units(gender_index, spl_index, resp_index) = num_of_units(gender_index, spl_index, resp_index) + 1;
+end 
+
+
+% Print Male data as table
+male_data = squeeze(num_of_units(1,:,:)); % length(dbs) x 3
+disp('Male data')
+disp(male_data)
+
+% Print Female data
+female_data = squeeze(num_of_units(2,:,:));
+disp('Female data')
+disp(female_data)
